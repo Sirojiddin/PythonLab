314711e5e1ab2338d0cee091f309515ff741219f
Sirojiddin Khamidov <sirojiddin89@gmail.com>
2023-12-03 13:28:56 +0500

    OS version

--- probe1.py
+++ probe1.py
@@ -1,2 +1,10 @@
-print("hello")
-print('hi')
+# TODO New probe
+import sys
+import platform
+
+info = 'Os info is \n{}\n\nPython version is {} {}'.format(
+    platform.uname(), sys.version, platform.architecture()
+)
+# print(info)
+with open('os_infi.txt','w') as ff:
+    ff.write(info)
